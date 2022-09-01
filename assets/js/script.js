9ec9787d597f74367b03e364e1cccdff65cabe29
desguerra <ddesguerra@ucdavis.edu>
2022-08-31 17:32:56 -0700

    add theme switcher slider

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,39 @@
 const footerCredEl = document.querySelector("#footer-credit");
+const toggleSwitch = document.querySelector('#theme-switch input[type="checkbox"]');
 
-// DATE - get current year for `footer`
+const currentTheme = localStorage.getItem('theme');
+
+/* THEME SLIDER */
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'whimsical') {
+        toggleSwitch.checked = true;
+    }
+}
+
+function switchTheme (event) {
+    if (event.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'whimsical');
+        localStorage.setItem('theme', 'whimsical');
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'mocha');
+        localStorage.setItem('theme', 'mocha');
+    }
+};
+
+/* DATE - get current year for `footer` */
 const displayCredit = () => {
     let currentYear =  new Date().getFullYear();
     let credit = document.createElement("span");
-    credit.innerHTML = `&copy; ${currentYear} - made with 🤍 by nikki esguerra`;
+    credit.innerHTML = `&copy; ${currentYear} - made with 🤍 by <span class="strong">nikki esguerra</span>`;
 
     footerCredEl.appendChild(credit);
 };
 
+/* CALL INITIAL FUNCTIONS */
 displayCredit();
+
+/* EVENT LISTENERS */
+toggleSwitch.addEventListener('change', switchTheme, false);
