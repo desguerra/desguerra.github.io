864104a6dfc9e516c6ad1530e33b547d1ae2506d
desguerra <ddesguerra@ucdavis.edu>
2022-09-17 13:33:44 -0700

    clean up code and update about me

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,40 +41,6 @@ function switchTheme() {
     setTheme();
 };
 
-/* DISPLAY PROJECTS ON PAGE */
-/* const displayProjects = () => {
-
-    console.log('testing...');
-
-    const columnContainer = document.createElement('div');
-    columnContainer.className = 'column col-sm-12 col-md-6 col-lg-4';
-
-    const articleEl = document.createElement('article');
-    articleEl.className = 'card text-center';
-
-    const thumnailEl = document.createElement('div');
-    thumnailEl.className = 'img';
-    const imgEl = document.createElement('img');
-    imgEl.className = 'card-img-top';
-    imgEl.setAttribute('src', './assets/images/project-thumbnails/p1.PNG');
-    imgEl.setAttribute('alt', 'project 1 thumbnail');
-
-    const cardBodyEl1 = document.createElement('div');
-    cardBodyEl1.className = 'card-body';
-    const cardTitleEl = document.createElement('h4');
-    cardTitleEl.className = 'card-title';
-    cardTitleEl.textContent = 'personal portfolio';
-    const cardPEl = document.createElement('p');
-    cardPEl.className = 'card-text';
-    cardPEl.textContent = 'the website you\'re looking at right now :)';
-
-    const cardUlEl = document.createElement('ul');
-    cardUlEl.className = 'list-group list-group-flush';
-    const cardLiEl = document.createElement('li');
-    cardLiEl.className = 'list-group-item';
-    
-}; */
-
 /* DATE - get current year for `footer` */
 const displayCredit = () => {
     let currentYear =  new Date().getFullYear();
